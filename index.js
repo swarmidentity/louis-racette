695622ee4dda8aaae4222ae807049288773bae88
SwarmIdentity <swarmidentity@gmail.com>
2022-02-04 12:23:32 -0300

    Starter template

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+// sendFile will go here
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+  });
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
