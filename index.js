71175db5c7e3a190ef1941e3b29f7eb007cd3d88
SwarmIdentity <swarmidentity@gmail.com>
2022-02-04 13:35:07 -0300

    Partially working history of coding languages

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
   });
 
+app.get('/joyplot_languages.html', function(req, res) {
+  res.sendFile(path.join(__dirname, '/joyplot_languages.html'));
+});
+
+app.get('/coding_language_history.csv', function(req, res) {
+  res.sendFile(path.join(__dirname, '/coding_language_history.csv'));
+});
+
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
 });
